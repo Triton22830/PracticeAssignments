6eef899298048d19bb266f5b882d0f8205f86440
Triton22830 <78033250+Triton22830@users.noreply.github.com>
2021-06-12 12:44:44 -0400

    Create countingCharacters.js

---
+++ countingCharacters.js
@@ -0,0 +1,4 @@
+function countingCharacters(stringToCount){
+  // THe length property has been mentiond in the logging letter exercise in lesson 4
+  console.log (stringToCount + "has" + stringToCount.length + "characters.");
+}
